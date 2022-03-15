91fd24321ae0c148754c3893afb1a412a0813abc
ivan966 <Ivan.parra96@hotmail.com>
2022-03-15 00:58:05 -0300

    registro

--- valan/src/app/app.module.ts
+++ valan/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import {  RouterModule} from '@angular/router';
-
+import { ReactiveFormsModule } from '@angular/forms';
 
 // permite hacer peticiones http 
 import { HttpClientModule } from '@angular/common/http';
@@ -51,7 +51,8 @@ import { SidenavListComponent } from './components/shared/sidenav-list/sidenav-l
     MatTabsModule,
     MatIconModule,
     MatButtonModule,
-    MatListModule
+    MatListModule,
+    ReactiveFormsModule
   ],
   exports: [
     MatSidenavModule,
